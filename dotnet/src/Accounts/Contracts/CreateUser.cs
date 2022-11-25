f21746ba79af0563e5477c06522c15d5f5c025cd
John Josua Paderon <johnjosua.paderon@outlook.com>
2022-11-25 14:13:43 +0800

    m_accounts: initial code

---
+++ dotnet/src/Accounts/Contracts/CreateUser.cs
@@ -0,0 +1,21 @@
+namespace JIL.Accounts.Contracts;
+
+public sealed record CreateUserCommand : IRequest
+{
+    public required string FirstName { get; init; }
+    public required string LastName { get; init; }
+    public required string Username { get; init; }
+    public required string CipherPassword { get; init; }
+
+    public sealed record Response : IResponse
+    {
+        public int Id { get; init; }
+    }
+}
+
+public sealed record UserCreatedEvent : MediatR.INotification
+{
+    public int Id { get; init; }
+    public required string FullName { get; init; }
+    public required string Username { get; init; }
+}
