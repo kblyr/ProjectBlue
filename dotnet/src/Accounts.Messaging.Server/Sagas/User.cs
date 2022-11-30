7933b16c3b5c556434dbab7d5dfbdd3d3632ab5d
John Josua Paderon <johnjosua.paderon@outlook.com>
2022-12-01 06:35:10 +0800

    feat: User Saga

--- dotnet/src/Accounts.Messaging.Server/Sagas/User.cs
+++ dotnet/src/Accounts.Messaging.Server/Sagas/User.cs
@@ -1,13 +1,56 @@
 #nullable disable
 
 using JIL;
+using JIL.Accounts.Lookups;
 using MassTransit;
+using Microsoft.Extensions.Options;
 
 public sealed class UserSaga : Saga<UserSaga.Instance>
 {
-    public State Created { get; private set; }
+    readonly UserStatusesLookup _statuses;
+
+    public UserSaga(IOptions<UserStatusesLookup> statuses)
+    {
+        _statuses = statuses.Value;
+    }
+
+    public State Pending { get; private set; }
+    public State Active { get; private set; }
+    public State Deactivated { get; private set; }
+    public State Locked { get; private set; }
 
     public Event<UserCreatedEvent> UserCreated { get; private set; }
+
+    protected override void ConfigureEvents()
+    {
+        Event(() => UserCreated,
+            corr => corr
+                .CorrelateBy((instance, context) => instance.Data.Id == context.Message.Id)
+                .SelectId(context => NewId.NextGuid())
+        );
+    }
+
+    protected override void ConfigureEventActivities()
+    {
+        Initially(
+            Ignore(UserCreated, context => context.Message.StatusId == _statuses.Deactivated),
+            Ignore(UserCreated, context => context.Message.StatusId == _statuses.Locked),
+            When(UserCreated, context => context.Message.StatusId == _statuses.Pending)
+                .Then(OnUserCreated)
+                .TransitionTo(Pending),
+            When(UserCreated, context => context.Message.StatusId == _statuses.Active)
+                .Then(OnUserCreated)
+                .TransitionTo(Active)
+        );
+    }
+
+    void OnUserCreated(BehaviorContext<Instance, UserCreatedEvent> context)
+    {
+        context.Saga.Data.Id = context.Message.Id;
+        context.Saga.Data.FullName = context.Message.FullName;
+        context.Saga.Data.Username = context.Message.Username;
+        context.Saga.Data.StatusId = context.Message.StatusId;
+    }
 
     public sealed class Instance : SagaInstance<Instance.DataObj>, ISagaInstance
     {
