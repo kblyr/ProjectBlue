3bc4cac2cd9657c183efe69ce679ebec94b6965a
John Josua Paderon <johnjosua.paderon@outlook.com>
2022-12-10 10:58:24 +0800

    feat: UserFullNameBuilder

--- dotnet/src/Accounts/Utilities/UserFullNameBuilder.cs
+++ dotnet/src/Accounts/Utilities/UserFullNameBuilder.cs
@@ -1,3 +1,5 @@
+using System.Text;
+
 namespace JIL.Accounts.Utilities;
 
 public interface IUserFullNameBuilder
@@ -9,4 +11,38 @@ public interface IUserFullNameSource
 {
     string FirstName { get; }
     string LastName { get; }
+}
+
+sealed class UserFullNameBuilder : IUserFullNameBuilder
+{
+    public string Build(IUserFullNameSource source)
+    {
+        if (source is null)
+        {
+            return "";
+        }
+
+        var firstName = source.FirstName.Trim();
+        var lastName = source.LastName.Trim();
+        var hasFirstName = !string.IsNullOrWhiteSpace(firstName);
+        var hasLastName = !string.IsNullOrWhiteSpace(lastName);
+        var builder = new StringBuilder();
+
+        if (hasFirstName)
+        {
+            builder.Append(firstName);
+
+            if (hasLastName)
+            {
+                builder.Append(" ");
+            }
+        }
+
+        if (hasLastName)
+        {
+            builder.Append(lastName);
+        }
+
+        return builder.ToString();
+    }
 }
