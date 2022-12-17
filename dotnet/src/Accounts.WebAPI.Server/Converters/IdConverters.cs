223d60c743148d6f2a2eb1c38bff9d7d3d4b6efe
John Josua Paderon <johnjosua.paderon@outlook.com>
2022-12-17 15:52:05 +0800

    Single UserIdConverter chore: remove UserIdConverter from JIL.Accounts.Converters

--- dotnet/src/Accounts.WebAPI.Server/Converters/IdConverters.cs
+++ dotnet/src/Accounts.WebAPI.Server/Converters/IdConverters.cs
@@ -1,4 +1,4 @@
-using JIL;
+using JIL.Converters;
 using Mapster;
 
 namespace JIL.Accounts.Converters;
@@ -6,11 +6,4 @@ namespace JIL.Accounts.Converters;
 static class MapConverters
 {
     public static UserIdConverter UserId => MapContext.Current.GetService<UserIdConverter>();
-}
-
-public sealed class UserIdConverter : Int32HashIdConverterBase, IHashIdConverter<int>
-{
-    public UserIdConverter(string salt, int minHashLength) : base(salt, minHashLength)
-    {
-    }
 }
